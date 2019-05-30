598aeb2498fdd2077c5ccc1cdc9d5091bfa51b3b
Emerson Bilieri Cluadelino <ebilieri@hotmail.com>
2019-05-30 11:48:09 -0300

    Add project files.

---
+++ WebApp/Controllers/AlunoController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class AlunoController : ApiController
+    {
+        // GET: api/Aluno
+        public IEnumerable<Alunos> Get()
+        {
+            Alunos aluno = new Alunos();
+
+            return aluno.ListaAlunos();
+        }
+
+        // GET: api/Aluno/5
+        public Alunos Get(int id)
+        {
+            Alunos aluno = new Alunos();
+
+            return aluno.ListaAlunos().Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        // POST: api/Aluno
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Aluno/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Aluno/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApp/Models/Alunos.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class Alunos
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+        public string Telefone { get; set; }
+        public int RA { get; set; }
+
+        public List<Alunos> ListaAlunos()
+        {
+            Alunos aluno = new Alunos();
+            aluno.Id = 1;
+            aluno.Nome = "Emerson";
+            aluno.SobreNome = "Bilieri";
+            aluno.Telefone = "123478";
+            aluno.RA = 1345;
+
+            Alunos aluno1 = new Alunos();
+            aluno1.Id = 2;
+            aluno1.Nome = "Emerson2";
+            aluno1.SobreNome = "Bilieri";
+            aluno1.Telefone = "123478";
+            aluno1.RA = 13452;
+
+            List<Alunos> listaAlunos = new List<Alunos>();
+            listaAlunos.Add(aluno);
+            listaAlunos.Add(aluno1);
+           
+            return listaAlunos;
+        }
+    }
+}
